1ee0b9a69c4139ac2fa89ae28b2f197e6e8c2865
p4rthk4 <hello@parthka.dev>
2024-06-10 22:19:50 +0530

    [init] u2smtp

---
+++ cmd/u2smtp.go
@@ -0,0 +1,10 @@
+// U2SMTP - server
+// 
+// Licensed under the MIT License for individual use or a commercial 
+// license for enterprise use. See LICENSE.txt and COMMERCIAL_LICENSE.txt.
+
+package main
+
+func main() {
+	
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/p4rthk4/u2smtp
+
+go 1.22.2

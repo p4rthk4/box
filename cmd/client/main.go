5a861c160a298667ddef1b98a027ce37e621853d
p4rthk4 <hello@parthka.dev>
2024-06-10 22:58:48 +0530

    [add] client and server file

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,4 +1,4 @@
-// U2SMTP - server
+// U2SMTP - client cmd
 // 
 // Licensed under the MIT License for individual use or a commercial 
 // license for enterprise use. See LICENSE.txt and COMMERCIAL_LICENSE.txt.
---
+++ cmd/server/main.go
@@ -0,0 +1,10 @@
+// U2SMTP - server cmd
+// 
+// Licensed under the MIT License for individual use or a commercial 
+// license for enterprise use. See LICENSE.txt and COMMERCIAL_LICENSE.txt.
+
+package main
+
+func main() {
+	
+}

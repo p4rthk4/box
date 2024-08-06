b3a30a41e1f46ef0d5e35130a90e5ff39e12b8c2
p4rthk4 <hello@parthka.dev>
2024-08-06 22:32:54 +0530

    test

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -16,8 +16,8 @@ func main() {
 	// try cockatielone.biz
 	// clinet.SetHost("alt3.gmail-smtp-in.l.google.com")
 	clinet.SetTimeout(5 * time.Second)
-	clinet.SetFrom("lolk@myworkspacel.ink")
-	clinet.SetRcpt("deg4m4@gmail.com")
+	clinet.SetFrom("parthdegama@myworkspacel.ink")
+	clinet.SetRcpt("parthka.2005@gmail.com")
 	clinet.SetHostname("mx.myworkspacel.ink")
 
 	// clinet.DSNReturn = smtpclient.DSNReturnFull
@@ -30,33 +30,32 @@ func main() {
 }
 
 var mail string = `DKIM-Signature: v=1; a=rsa-sha256; c=relaxed/relaxed; d=myworkspacel.ink;
-        q=dns/txt; s=draft; bh=ujNdhHpDGWw+POIbrDZgZUuaWfr351c6LCnwhuqZx+c=;
+        q=dns/txt; s=draft; bh=10ksEvlgKQRSmdzeDKsGBQ3BXzRiT4pq9KuMlBwqjqg=;
         h=from:subject:to:mime-version:content-type;
-        b=JikMxbuwqYh0ZVsc9IQesnqsW9ZDKaOoC4ibTITqTVGqvo3QZepcox8NoiU8K1rY9l2uIXxnj
-         JfZmoTZFeHKg2WcbNlE7w2LdpXdn6YfGn/eej10GXN9wt5M0USVrWcJDRrK/RTDLXCZZs0f8ATq
-         0nbRVsl3mNUiRWQP4X0dNHZwqIXYxV+C42gGzgBk/UppFZfdkhcLw0XwxEU3GJBVtEwhqHZ8crx
-         L/mBOt51osBePtiePN7BaKts6Wj/Va66Iv7uNn1rEql7Y5aZ9iFicZlozfdFo94xJHnWdq9Oa3O
-         LteVs7V1hYj+OTtipOhYSJayJ3PQJp0SXyQxnnq6dCSw==
-From: lolk@myworkspacel.ink
-To: deg4m4@gmail.com
+        b=SkFp6oV3PBAguAGQE64B8SF1jUhuWXqdFxK6V0STCl3Iriey8DcPkASHqkJulE/C2CD6l/ys/
+         PvaEAggikO8heKu2v2t5qrxANSYmISwj/Lva1Gza508neHbepruMbbRW2xMw7JuSIRn6jB8k9mI
+         JolzwXWKkonluRI4yalqCAxCmRfMIafi6seliyW/0g6ewGqyUKYFtS/++OhEqTEZVAo//5uI5Cw
+         bxqhsj33Pcw5ei0ZzsZINLFpqiCLAULejqEp6mbSllrbP1frQfOxJjrK88/0GOqQubCYYmXIshG
+         jZzFqp4uWdjaZNrcgLAtnWqYPpKwzhjumHgmQISXYJBg==
+From: parthdegama@myworkspacel.ink
+To: parthka.2005@gmail.com
 Subject: Test Message!
-Message-ID: <d1cd7af0-42e8-dcda-5415-c831ee005e20@myworkspacel.ink>
-Date: Tue, 06 Aug 2024 16:55:50 +0000
+Message-ID: <74c858e5-79bf-f1ea-8334-111f39b67901@myworkspacel.ink>
+Date: Tue, 06 Aug 2024 17:01:25 +0000
 MIME-Version: 1.0
 Content-Type: multipart/alternative;
- boundary="--_NmP-fba039d6714fd8a7-Part_1"
+ boundary="--_NmP-2bfb0752aee9fb57-Part_1"
 
-----_NmP-fba039d6714fd8a7-Part_1
+----_NmP-2bfb0752aee9fb57-Part_1
 Content-Type: text/plain; charset=utf-8
 Content-Transfer-Encoding: 7bit
 
 Hello World
-----_NmP-fba039d6714fd8a7-Part_1
+----_NmP-2bfb0752aee9fb57-Part_1
 Content-Type: text/html; charset=utf-8
 Content-Transfer-Encoding: 7bit
 
 <h1>Hello!</h1>
-----_NmP-fba039d6714fd8a7-Part_1--
-
+----_NmP-2bfb0752aee9fb57-Part_1--
 
 `

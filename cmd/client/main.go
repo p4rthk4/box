ec41c7961132f6dfbb9674d5c41830d5aebca76f
p4rthk4 <hello@parthka.dev>
2024-08-06 21:48:27 +0530

    [test]

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -30,31 +30,31 @@ func main() {
 }
 
 var mail string = `DKIM-Signature: v=1; a=rsa-sha256; c=relaxed/relaxed; d=myworkspacel.ink;
-	q=dns/txt; s=draft; bh=ddgIlflXn5KXzZYAkEbeLhBKFYGaZHohlzlMrIoaCzo=;
-	h=from:subject:to:mime-version:content-type;
-	b=kV9LYU1Vytq5UcquD8ZKCUGBVmqy3nYE0qfzIlqP9og5oYd2JSa89+eB9xE6lztODW6jtodDp
-	 cilS9tcA9ko1uKLFWZ0Gj1FSEtitNLQY6/Y48TDeQXDTmuwOaf4YkRpIPIGRSKpQWWNWPYhglaD
-	 HpOYLt/JvZNhfmwSORPbE6s=
+ q=dns/txt; s=draft; bh=UN1Luk0CfA92T8/KKG0F5isLo3kzJdhmFjMztTyXB40=;
+ h=from:subject:to:mime-version:content-type;
+ b=kDH+tTQLJWmFgs3DLszQ2u8kmxzay9A0I9xWIV3oQn+hA83Xr0FgOAWYPlBqcr2RwBvWOcRjb
+ 5fIpuF8Q1eWwDAteQWrewSAKzrvHlFLaBlTUoPPT4UrC034+bZwkAE8JFnXlXi02zNxE+OBGYDn
+ sV/G/LC340A5CpegERri40TEk1RA6aY8LxkRJ5l/kODrP8X5c/sLM9kOukAxEO9Oxfq3opPbAQ+
+ ALXg7KOrcffriHci8bLM7taPISRvGDpUI1Vn8U7dmHydVWU9pwnghxhOaB2V7XDECuTJUqvzGDH
+ +FLx6v+qsZYHbj7i3IxFWywQlFUOhsKL0/fykZOK2+vw==
 From: parthka@myworkspacel.ink
 To: Parth <parthka.2005@gmail.com>
 Subject: Test Message!
-Message-ID: <7dc4a9e5-5e35-0e02-6edd-1b53e4c46e5d@myworkspacel.ink>
-Date: Tue, 06 Aug 2024 16:00:25 +0000
+Message-ID: <32ff42dc-d901-16e0-6c5a-2c1b5baf1843@myworkspacel.ink>
+Date: Tue, 06 Aug 2024 16:17:03 +0000
 MIME-Version: 1.0
 Content-Type: multipart/alternative;
- boundary="--_NmP-a6e2095a9a0e0ea4-Part_1"
+ boundary="--_NmP-5e2249bfe0044c95-Part_1"
 
-----_NmP-a6e2095a9a0e0ea4-Part_1
+----_NmP-5e2249bfe0044c95-Part_1
 Content-Type: text/plain; charset=utf-8
 Content-Transfer-Encoding: 7bit
 
 Hello World
-----_NmP-a6e2095a9a0e0ea4-Part_1
+----_NmP-5e2249bfe0044c95-Part_1
 Content-Type: text/html; charset=utf-8
 Content-Transfer-Encoding: 7bit
 
 <h1>Hello!</h1>
-----_NmP-a6e2095a9a0e0ea4-Part_1--
-
-
+----_NmP-5e2249bfe0044c95-Part_1--
 `

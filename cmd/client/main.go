6ddb40fc052477fcb8552f99b6e2a49242cda3ee
p4rthk4 <hello@parthka.dev>
2024-08-07 09:44:21 +0530

    add

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -17,7 +17,7 @@ func main() {
 	// clinet.SetHost("alt3.gmail-smtp-in.l.google.com")
 	clinet.SetTimeout(5 * time.Second)
 	clinet.SetFrom("parthdegama@cockatielone.biz")
-	clinet.SetRcpt("parthka.2005@gmail.com")
+	clinet.SetRcpt("pthreeh@outlook.com")
 	clinet.SetHostname("mx.myworkspacel.ink")
 
 	// clinet.DSNReturn = smtpclient.DSNReturnFull
@@ -30,27 +30,27 @@ func main() {
 }
 
 var mail string = `DKIM-Signature: v=1; a=rsa-sha256; c=relaxed/relaxed; d=cockatielone.biz;
- q=dns/txt; s=draft; bh=/He0rngpZD20KignV/pZ1EOiNFQ4twVmG8JfDoFc6LQ=;
+ q=dns/txt; s=draft; bh=olxGR7wvqmpGCBHsxWlk5wgwYwId+jmy2Zz3NqRgoFo=;
  h=from:subject:to:mime-version:content-type;
- b=ZYV6V8rMzYcI8nym0RjZPh+JRrgNcE+Kob0D1/4B5mxkcdW1tr9m+WPmHCQ5oEP5rzO5lOfKG
- m4JVA5c2FQFvCiIu0hyyxycZw2bAxA+I1cMPtcVLR59UYVJI8tmpbnL7hycKyTMCIP70aBgVerL
- gMKCJIwShQFNEFDw8nHq9wksWpXt7kiJ4Sr69+32MRyDh2+zrdGbcAepPzkt74btk7DFQ1O2+c6
- Km+d5MoPYHE6i5S9qLojpGIMmtgG0EAJunQ1jo0bIauISwUWStJivDxbs3XOVTZ/PfT8zVP250T
- 39XyG8n8OENUmo4niA9w1vxJqBfuhlJXHCrD5VEboYFQ==
+ b=cNy3zuVXparY6apEr7xIMWfhO3gTIXMIQiS5upB+kjEi4RD7o+dAO/Mu6GiyKzLRqhu3etzu2
+ D3MkVNd92yZ9GI+gMUajvvWPJycYMdD4S0pDEuczdshdVJ7PL2Fju+8eu8QX1PTreMmJUeJcxWw
+ k8D7Rw0t7+DeujdZOOzmjOGhzpX7qsdyOksltxVEvw4l3e5WERpc6wtPFWtlY2AXdiiR+eRgY2L
+ X6uTqDu5Wu+NLoqzrXswopTPYqG7sAjncjdTAM2lsVPdoC52YpN3HtqM64cagLFkta1tCqX7FK8
+ w+avn4FuRyJym9gGHkBki8miRYvHzMnEa3/s4sIF5eUw==
 Content-Type: multipart/alternative;
- boundary="----sinikael-?=_1-17230038801180.23133196497826436"
+ boundary="----sinikael-?=_1-17230040309390.004235149441492503"
 From: PARTH <parthdegama@cockatielone.biz>
-To: Parthka <parthka.2005@gmail.com>
+To: Parthka <pthreeh@outlook.com>
 Subject: Hello Test Message From Parth!
-Date: Wed, 07 Aug 2024 04:11:20 +0000
-Message-Id: <1723003880120-2db85b81-708ee516-1ca6f483@cockatielone.biz>
+Date: Wed, 07 Aug 2024 04:13:50 +0000
+Message-Id: <1723004030941-22df59a5-8ce462be-8f469442@cockatielone.biz>
 MIME-Version: 1.0
 
-------sinikael-?=_1-17230038801180.23133196497826436
+------sinikael-?=_1-17230040309390.004235149441492503
 Content-Type: text/plain
 Content-Transfer-Encoding: 7bit
 
 Hello World!, my name is parth degama and your name
-------sinikael-?=_1-17230038801180.23133196497826436--
+------sinikael-?=_1-17230040309390.004235149441492503--
 
 `

c3277e3374180089efbeeedecccd95d57df674b9
p4rthk4 <hello@parthka.dev>
2024-08-07 08:50:37 +0530

    test

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -17,7 +17,7 @@ func main() {
 	// clinet.SetHost("alt3.gmail-smtp-in.l.google.com")
 	clinet.SetTimeout(5 * time.Second)
 	clinet.SetFrom("lopel111@cockatielone.biz")
-	clinet.SetRcpt("parthka.2005@gmail.com")
+	clinet.SetRcpt("parthka.2005@proton.me")
 	clinet.SetHostname("mx.myworkspacel.ink")
 
 	// clinet.DSNReturn = smtpclient.DSNReturnFull
@@ -30,32 +30,32 @@ func main() {
 }
 
 var mail string = `DKIM-Signature: v=1; a=rsa-sha256; c=relaxed/relaxed; d=cockatielone.biz;
- q=dns/txt; s=draft; bh=ZaFb4HjAGQRw753RecXgkuWa1t0gTDMY+HrG2YvFZ70=;
+ q=dns/txt; s=draft; bh=s+LisPZ/kV8OBCx+/82+4XwlUr7IYG1h+a1OXFmrcUQ=;
  h=from:subject:to:mime-version:content-type;
- b=ig2r9FPSBajz9iTt/rwWByInmAyzd7rrqohfvUB1N1VkQscwJ3d/vFjzhKLpJiII9arXhxUDm
- y/nlkHjqwVHd5m5zp6ljCPWtZ9dYNBVJW5dGQwlBrDnsjCBQkRuDd1dOWOi1Zbc0KVyc0IyMb72
- nB24AU47HSMOi2r7W+wffSf7vTtB+jBjo3uu1eo339J6U5qLx6rCNgWk0Fg9e1d0RovhsA5Vb/y
- kWOsgaVg2pFsgKyitRoSA9NawzTc72Pc0YORrcjxWvEaxM2TrU76jTQpTVKlSMGsGpIPnWyIWOp
- eZey3DFkE1ODba4ak+DrApMCOG4LWcfT1vWmrMs+jGGg==
-From: lopel111@cockatielone.biz
-To: parthka.2005@gmail.com
+ b=R8KZBpBOL5mYiuiKOFtF6dpOrqKY8NXg144Wi/kMAXXJElx+Ot0nBJnuNcd2OfGg40Yy14wRk
+ KEYnBWPh97z0gnDmOi4vy1ijS4neJ7XQW9Op0DqY59IvgkO/4qZ5RMxLbCo1gsk2RPU8ihmsFOz
+ VI3lWWA8Ile973W6SCU8kV24tKJTQxy9o1p5eNHgyGe4+MIMjaafDUGili3WgkG1TZfdos+19EE
+ O0ptnC53xSMuidECfWkvFWAZbrD4VcZGIcipIIX6nyyif3R5nMfsHAzqpSJ9U7MqeaBT4atLZID
+ LZoq+koklgA+eCWkna0G9AjuyA7jHoWZfDWwwp94tGBw==
+From: lo🤑pel111@cockatielone.biz
+To: parthka.2005@proton.me
 Subject: Hello Test Message!
-Message-ID: <c2387efe-6915-d0ff-b9ee-fdaea1c125e8@cockatielone.biz>
-Date: Wed, 07 Aug 2024 03:18:46 +0000
+Message-ID: <cb4db153-9dc9-65e9-492a-aabee9d7b730@cockatielone.biz>
+Date: Wed, 07 Aug 2024 03:20:17 +0000
 MIME-Version: 1.0
 Content-Type: multipart/alternative;
- boundary="--_NmP-c19e3b851409c4e5-Part_1"
+ boundary="--_NmP-7477e98eb12c786a-Part_1"
 
-----_NmP-c19e3b851409c4e5-Part_1
+----_NmP-7477e98eb12c786a-Part_1
 Content-Type: text/plain; charset=utf-8
 Content-Transfer-Encoding: quoted-printable
 
 Hello World =F0=9F=A4=91=F0=9F=A4=91
-----_NmP-c19e3b851409c4e5-Part_1
+----_NmP-7477e98eb12c786a-Part_1
 Content-Type: text/html; charset=utf-8
 Content-Transfer-Encoding: 7bit
 
 <h1>Hello!</h1>
-----_NmP-c19e3b851409c4e5-Part_1--
+----_NmP-7477e98eb12c786a-Part_1--
 
 `

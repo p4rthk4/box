5649ff18255d3c98f6b481aabd591e613ac966be
p4rthk4 <hello@parthka.dev>
2024-08-07 10:08:06 +0530

    test

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -36,19 +36,20 @@ Subject: Hello Test Message From Parth!
 Message-ID: <139244aa-5f4d-4a08-67d5-db284cefab61@cockatielone.biz>
 Date: Wed, 07 Aug 2024 04:34:52 +0000
 MIME-Version: 1.0
-Content-Type: multipart/alternative;
- boundary="--_NmP-3e34254ed23852ad-Part_1"
+Content-Type: multipart/alternative; boundary="--_NmP-3e34254ed23852ad-Part_1"
 
 ----_NmP-3e34254ed23852ad-Part_1
 Content-Type: text/plain; charset=utf-8
 Content-Transfer-Encoding: base64
 
 SGVsbG8gV29ybGQhLCBteSBuYW1lIGlzIHBhcnRoIGRlZ2FtYSBhbmQgeW91ciBuYW1l
+
 ----_NmP-3e34254ed23852ad-Part_1
 Content-Type: text/html; charset=utf-8
 Content-Transfer-Encoding: base64
 
 PGI+SGVsbG8gV29ybGQhLCBteSBuYW1lIGlzIHBhcnRoIGRlZ2FtYSBhbmQgeW91ciBuYW1lPC9i
 PiAgIA==
+
 ----_NmP-3e34254ed23852ad-Part_1--
 `

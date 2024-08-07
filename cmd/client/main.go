b86ea501c00d80e9a4a38b302867faf48e2844a2
p4rthk4 <hello@parthka.dev>
2024-08-07 11:56:57 +0530

    test

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -17,8 +17,8 @@ func main() {
 	// try cockatielone.biz
 	// clinet.SetHost("alt3.gmail-smtp-in.l.google.com")
 	clinet.SetTimeout(5 * time.Second)
-	clinet.SetFrom("parthdegama@cockatielone.biz")
-	clinet.SetRcpt("pthreeh@outlook.com")
+	clinet.SetFrom("aly@cockatielone.biz")
+	clinet.SetRcpt("hello@parthka.dev")
 	clinet.SetHostname("mx.myworkspacel.ink")
 
 	// clinet.DSNReturn = smtpclient.DSNReturnFull
@@ -35,33 +35,33 @@ func main() {
 }
 
 var mail string = `DKIM-Signature: v=1; a=rsa-sha256; c=relaxed/relaxed; d=cockatielone.biz;
- q=dns/txt; s=draft; bh=/W9YZjqmeW2SEYNlLZjjGzd//yNcXYtg/3+BADllX+Q=;
+ q=dns/txt; s=draft; bh=syf40hXwgZLxjCerTMBtjRy78eOxi8/4c07t9KzYT70=;
  h=from:subject:to:mime-version:content-type;
- b=T4Dr0uoR5IFYWKgwoK4wjXlr0BcQHxgeAdh1j0XGOeyOg6fVPhLHGmUYV1miSGNvBFEqugtaC
- w7wJEoBe1NUgQs2/D+18bRHD/Amv7pUVf8KWvbIHkt26qhYZKzWv2sF9upxIVPiD9rdFtFYAhqq
- 4LbgbZm7vXdTIpXgdBUoD1/TYMhyxi2m3PP3KXLsWY7+6qJB3obQZhtZk8tY3EDAqFfEzJ+9pkC
- LjIsfI9B86iuKG0IldGGW+GBRlB87alpAclZugmvfg2g6LZbZcWt9rfaN1KUtzeljUzWD1zknLn
- qlin2AUjvNH1BCQXWe3NiG+iA7naDC2irj1UrPIlDUhg==
-From: PARTH <parthdegama@cockatielone.biz>
-To: Parthka <pthreeh@outlook.com>
-Subject: Hello Test Message From India!
-Message-ID: <652f6881-662e-ce06-9f3f-1bddd2c8af3a@cockatielone.biz>
-Date: Wed, 07 Aug 2024 05:04:12 +0000
+ b=gHvhZj2wr/YrTbjqnhCJAqlZrFG3lNZRkMfmYPJQMQ5BquVU+UZA58XP2Z0ORPdlMeSM5tBOY
+ 0k/ARX41nZvqhydFAczVbOAu2okA5lje1A9vqyVYw3/0lIaxVvhGUfRWf7j9grBSBrNDehC6sXD
+ aUtPyLEpHxReSNygV8ZJXDRhbAnp2COWPBEqDU7EURUqGwWM9CPgYsOJ1aap4J8zX1P7EDJtKu8
+ 8PtexWm4/dFUPw+716YbLcOI3n2xPZMinw7I9O+QG54KHuqRGAUU4dTWv4hx95y8KZktGxBG5Ph
+ q7RfLuFLuDeAQ7DZqCNx40sM5DuieVZ9leAzdBiI36nQ==
+From: PARTH <aly@cockatielone.biz>
+To: Parthka <hello@parthka.dev>
+Subject: Final Helo Mail
+Message-ID: <49625f91-b609-6619-0164-1177052f5cb9@cockatielone.biz>
+Date: Wed, 07 Aug 2024 06:24:53 +0000
 MIME-Version: 1.0
 Content-Type: multipart/alternative;
- boundary="--_NmP-a192fa3e5098ee4e-Part_1"
+ boundary="--_NmP-b2260b694d6fd9b8-Part_1"
 
-----_NmP-a192fa3e5098ee4e-Part_1
+----_NmP-b2260b694d6fd9b8-Part_1
 Content-Type: text/plain; charset=utf-8
 Content-Transfer-Encoding: base64
 
 SGVsbG8gV29ybGQhLCBteSBuYW1lIGlzIHBhcnRoIGRlZ2FtYSBhbmQgeW91ciBuYW1l
-----_NmP-a192fa3e5098ee4e-Part_1
+----_NmP-b2260b694d6fd9b8-Part_1
 Content-Type: text/html; charset=utf-8
 Content-Transfer-Encoding: base64
 
 PGgxPjxiPkhlbGxvIFdvcmxkISwgbXkgbmFtZSBpcyBwYXJ0aCBkZWdhbWEgYW5kIHlvdXIgbmFt
 ZTwvYj48L2gxPg==
-----_NmP-a192fa3e5098ee4e-Part_1--
+----_NmP-b2260b694d6fd9b8-Part_1--
 
 `

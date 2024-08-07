89cec2a9d7aede99e298da252f65243c24934c1f
p4rthk4 <hello@parthka.dev>
2024-08-07 10:04:04 +0530

    test

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -29,30 +29,15 @@ func main() {
 	fmt.Println(err)
 }
 
-var mail string = `DKIM-Signature: v=1; a=rsa-sha256; c=relaxed/relaxed; d=cockatielone.biz;
- q=dns/txt; s=draft; bh=MmGHooEuTk74lX8yQJXyZbJE4JwzASjnQOkoeF2bb2k=;
- h=from:subject:to:mime-version:content-type;
- b=OHBmNdo4zEY7bU9DT/uP9/3B17Y0lFdZ/10G/5f7T/AEPr6wIsmdhstGRs9ff/5Skpfq5ugqQ
- atVbFoWYHCENMhaHoIqVqRXtOjWDjL1WplgJS+HurEgR4A1ztGORwbG+d3WeCz3taf5z+vx9tGz
- cmk5/Z+LQwLAOdRpnUQMWZUfUVkMAPtRlHXvoRlw/fX4R5GOFcMEl4HGljQnxmWmX0HmDNeajqh
- yWKazRdnWw46q3KPsdSN/OCzmsnnKqCNU9bdHw+AjRjO/1izjM4QcE7Yo2GOWr313BVPNdmk/2k
- rzdGaAKV7xwbT1TOXO0xVReigePyRL9uZBoU7JJZJv3Q==
-Content-Type: multipart/alternative;
- boundary="----sinikael-?=_1-17230042791050.9117167673958415"
+var mail string = `Content-Transfer-Encoding: base64
 From: PARTH <parthdegama@cockatielone.biz>
 To: Parthka <pthreeh@outlook.com>
 Subject: Hello Test Message From Parth!
-Date: Wed, 07 Aug 2024 04:17:59 +0000
-Message-Id: <1723004279108-5f956586-43ce2ed3-04b9ea31@cockatielone.biz>
+Message-ID: <a9a7d245-9e9a-d866-658d-a62c46c525ff@cockatielone.biz>
+Date: Wed, 07 Aug 2024 04:33:44 +0000
 MIME-Version: 1.0
-
-------sinikael-?=_1-17230042791050.9117167673958415
 Content-Type: text/plain; charset=utf-8
-Content-Transfer-Encoding: base64
 
-U0dWc2JHOGdWMjl5YkdRaExDQnRlU0J1WVcxbElHbHpJSEJoY25Sb0lHUmxaMkZ0WVNCaGJtUWdl
-VzkxY2lCdVlXMWw=
-
-------sinikael-?=_1-17230042791050.9117167673958415--
+SGVsbG8gV29ybGQhLCBteSBuYW1lIGlzIHBhcnRoIGRlZ2FtYSBhbmQgeW91ciBuYW1l
 
 `

bc29ac0ed976429324aa8c82d023e4212e7ecfd7
p4rthk4 <hello@parthka.dev>
2024-08-07 09:48:27 +0530

    test

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -30,27 +30,28 @@ func main() {
 }
 
 var mail string = `DKIM-Signature: v=1; a=rsa-sha256; c=relaxed/relaxed; d=cockatielone.biz;
- q=dns/txt; s=draft; bh=olxGR7wvqmpGCBHsxWlk5wgwYwId+jmy2Zz3NqRgoFo=;
+ q=dns/txt; s=draft; bh=MmGHooEuTk74lX8yQJXyZbJE4JwzASjnQOkoeF2bb2k=;
  h=from:subject:to:mime-version:content-type;
- b=cNy3zuVXparY6apEr7xIMWfhO3gTIXMIQiS5upB+kjEi4RD7o+dAO/Mu6GiyKzLRqhu3etzu2
- D3MkVNd92yZ9GI+gMUajvvWPJycYMdD4S0pDEuczdshdVJ7PL2Fju+8eu8QX1PTreMmJUeJcxWw
- k8D7Rw0t7+DeujdZOOzmjOGhzpX7qsdyOksltxVEvw4l3e5WERpc6wtPFWtlY2AXdiiR+eRgY2L
- X6uTqDu5Wu+NLoqzrXswopTPYqG7sAjncjdTAM2lsVPdoC52YpN3HtqM64cagLFkta1tCqX7FK8
- w+avn4FuRyJym9gGHkBki8miRYvHzMnEa3/s4sIF5eUw==
+ b=OHBmNdo4zEY7bU9DT/uP9/3B17Y0lFdZ/10G/5f7T/AEPr6wIsmdhstGRs9ff/5Skpfq5ugqQ
+ atVbFoWYHCENMhaHoIqVqRXtOjWDjL1WplgJS+HurEgR4A1ztGORwbG+d3WeCz3taf5z+vx9tGz
+ cmk5/Z+LQwLAOdRpnUQMWZUfUVkMAPtRlHXvoRlw/fX4R5GOFcMEl4HGljQnxmWmX0HmDNeajqh
+ yWKazRdnWw46q3KPsdSN/OCzmsnnKqCNU9bdHw+AjRjO/1izjM4QcE7Yo2GOWr313BVPNdmk/2k
+ rzdGaAKV7xwbT1TOXO0xVReigePyRL9uZBoU7JJZJv3Q==
 Content-Type: multipart/alternative;
- boundary="----sinikael-?=_1-17230040309390.004235149441492503"
+ boundary="----sinikael-?=_1-17230042791050.9117167673958415"
 From: PARTH <parthdegama@cockatielone.biz>
 To: Parthka <pthreeh@outlook.com>
 Subject: Hello Test Message From Parth!
-Date: Wed, 07 Aug 2024 04:13:50 +0000
-Message-Id: <1723004030941-22df59a5-8ce462be-8f469442@cockatielone.biz>
+Date: Wed, 07 Aug 2024 04:17:59 +0000
+Message-Id: <1723004279108-5f956586-43ce2ed3-04b9ea31@cockatielone.biz>
 MIME-Version: 1.0
 
-------sinikael-?=_1-17230040309390.004235149441492503
-Content-Type: text/plain
-Content-Transfer-Encoding: 7bit
+------sinikael-?=_1-17230042791050.9117167673958415
+Content-Type: text/plain; charset=utf-8
+Content-Transfer-Encoding: base64
 
-Hello World!, my name is parth degama and your name
-------sinikael-?=_1-17230040309390.004235149441492503--
+U0dWc2JHOGdWMjl5YkdRaExDQnRlU0J1WVcxbElHbHpJSEJoY25Sb0lHUmxaMkZ0WVNCaGJtUWdl
+VzkxY2lCdVlXMWw=
+------sinikael-?=_1-17230042791050.9117167673958415--
 
 `

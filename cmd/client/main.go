9e0d2d5b01bfb752699bf6d4b2dd33611cbce42d
p4rthk4 <hello@parthka.dev>
2024-08-07 10:24:08 +0530

    test

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -32,20 +32,22 @@ func main() {
 var mail string = `From: PARTH <parthdegama@cockatielone.biz>
 To: Parthka <pthreeh@outlook.com>
 Subject: Hello Test Message From Parth!
-Message-ID: <b1f7e0c1-331f-eca6-a633-d724e596da43@cockatielone.biz>
-Date: Wed, 07 Aug 2024 04:42:33 +0000
+Message-ID: <6416171d-08bc-16b7-c388-f1b22f96ae9b@cockatielone.biz>
+Date: Wed, 07 Aug 2024 04:52:46 +0000
 MIME-Version: 1.0
-Content-Type: multipart/alternative; boundary="000000000000ddaf2c061f104677"
+Content-Type: multipart/alternative;
+ boundary="b1_ef8joBCfkrkh2RYftVW8WJCb3gWV1cA5gpuUeEGtDHA"
 
---000000000000ddaf2c061f104677
-Content-Type: text/plain; charset="UTF-8"
+--b1_ef8joBCfkrkh2RYftVW8WJCb3gWV1cA5gpuUeEGtDHA
+Content-Type: text/plain; charset=utf-8
+Content-Transfer-Encoding: base64
 
-I Am *Parth* Degama
+SGVsbG8gV29ybGQhLCBteSBuYW1lIGlzIHBhcnRoIGRlZ2FtYSBhbmQgeW91ciBuYW1l
+--b1_ef8joBCfkrkh2RYftVW8WJCb3gWV1cA5gpuUeEGtDHA
+Content-Type: text/html; charset=utf-8
+Content-Transfer-Encoding: base64
 
---000000000000ddaf2c061f104677
-Content-Type: text/html; charset="UTF-8"
-
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"><div dir="ltr"><div class="gmail_default" style="font-family:arial,sans-serif">I Am <b>Parth</b> Degama</div></div>
-
---000000000000ddaf2c061f104677--
+PGI+SGVsbG8gV29ybGQhLCBteSBuYW1lIGlzIHBhcnRoIGRlZ2FtYSBhbmQgeW91ciBuYW1lPC9i
+Pg==
+--b1_ef8joBCfkrkh2RYftVW8WJCb3gWV1cA5gpuUeEGtDHA--
 `

a7061a897668b18185712f254e30d7a208fba5bc
p4rthk4 <hello@parthka.dev>
2024-08-07 09:16:33 +0530

    test

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -17,7 +17,7 @@ func main() {
 	// clinet.SetHost("alt3.gmail-smtp-in.l.google.com")
 	clinet.SetTimeout(5 * time.Second)
 	clinet.SetFrom("parthdegama@cockatielone.biz")
-	clinet.SetRcpt("parthka.2005@gmail.com")
+	clinet.SetRcpt("pthreeh@outlook.com")
 	clinet.SetHostname("mx.myworkspacel.ink")
 
 	// clinet.DSNReturn = smtpclient.DSNReturnFull
@@ -32,25 +32,19 @@ func main() {
 var mail string = `DKIM-Signature: v=1; a=rsa-sha256; c=relaxed/relaxed; d=cockatielone.biz;
  q=dns/txt; s=draft; bh=PKtoTjqHuudr64nYpizw9stW9CenBfl0sXHFI8KEfBA=;
  h=from:subject:to:mime-version:content-type:content-transfer-encoding;
- b=j57fHsYYj7BVRbZ++sxSmJyCR5d+XHsax5F6Dk1kYdapSOh3DjNUbLFMp8HDRELuz+LHgHlVc
- HFNPdOcO/lHZBis2DoI0+3etH1A1/QXhJGkgYv6ACdJpYVvDyKKs5uCMVDB9t/XyhJVzRLvFN1S
- 9yN9CTCL3Mfv2EmGzWna9+wcxmWTNevSTeme9NgmUr9JClo1cpPVMoDmPCCN4/2QpvNepMuCeR3
- o732Zce2FKgN7nOXVwBHt7JjQTCskwYTsTsPDu/bEv+gSv0eQgc063lcklxQ+ginBpeXV3fUrt0
- rF6YaKg9GVxHA5ApHTWAjmDv8Ykw68k/b6TE52b3Yr6w==
+ b=FyH1Vt0URXqWlRQz72cMnDs5U4De1YzU0CAVgVj1+dXpinlvXS5/UviNIg3tzBS7KP1gCjiK0
+ xgaQ+TpBNezL+YKenR0wCC+YZDtgq/QVOrUlogVTzfkOQE+QPjs0YaVULN/t6y5VRjBz/JXIuBf
+ v/ADu0T2allxJXh6yLtorP8S8dwjvov4zJIIN5oKwXylKIAbfYd3SQ76JLEzPGjQ4neBpEC1eD1
+ cBbG2NnJIbe4B75CYOUJDv0jri5vvDRX7cAaQNuW4cUfaOyg02s9MsANWlIkZHfN7uDhJ86CsCV
+ sUExIeN1ZKtAJsywB1UEeG0LbYBCOlIhVxMzV2/lua2A==
 From: parthdegama@cockatielone.biz
-To: parthka.2005@gmail.com
+To: pthreeh@outlook.com
 Subject: Hello Test Message From Parth!
-Message-ID: <4e1ec5df-75b9-a6ac-055a-c9c9d4933b1b@cockatielone.biz>
+Message-ID: <7a20877a-ec53-0dd0-cd36-624af1c4d146@cockatielone.biz>
 Content-Transfer-Encoding: 7bit
-Date: Wed, 07 Aug 2024 03:44:08 +0000
+Date: Wed, 07 Aug 2024 03:46:06 +0000
 MIME-Version: 1.0
 Content-Type: text/plain; charset=utf-8
 
 Hello World!, my name is parth degama and your name
-
-
-
-
-
-
 `

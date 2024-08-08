1fd7e1dce4fc532124b08a8be56ab014f3f048ed
p4rthk4 <hello@parthka.dev>
2024-08-08 15:29:55 +0530

    [remove] server sonfig

--- pkg/server/preprocess.go
+++ pkg/server/preprocess.go
@@ -57,15 +57,7 @@ func smtpServerPreProcess(l *logx.Log) {
 		}
 		tlsConfig = &tls.Config{
 			Certificates:             []tls.Certificate{cert},
-			MinVersion:               tls.VersionTLS12,
-			CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
-			PreferServerCipherSuites: true,
-			CipherSuites: []uint16{
-				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-				tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
-			},
+			MinVersion:               tls.VersionTLS10,
 		}
 	}
 }

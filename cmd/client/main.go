cf19343ea658887dbfda104ab79be822d43ed6f6
p4rthk4 <hello@parthka.dev>
2024-08-10 07:38:52 +0530

    test

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -52,33 +52,32 @@ func main() {
 }
 
 var mail string = `DKIM-Signature: v=1; a=rsa-sha256; c=relaxed/relaxed; d=cockatielone.biz;
- q=dns/txt; s=draft; bh=qsJaBTlVweNE/meK3+bX5jaRZpKJ8Ym17OllQAAQWmg=;
+ q=dns/txt; s=draft; bh=TeIFrF74hDBEkjt9OeUkEQZM+ljjmJFn59ulnGw49JQ=;
  h=from:subject:to:mime-version:content-type;
- b=Ei3hPdKWQT26APqldl5VUJ9fFW/KJqzSbSYqVt4LVyE6qZx5gLcC7YCbmXDjydKWvXKfEkxfK
- ruhHnJUhz/Gn9EzJM2JhoPgGG6sAt7gG2FccFlwUseyZvbTN+dF1S2pUe2hn96pv1zFX/0D8ptg
- dkMc5IMi4sBp/GCuMxC3vUes12anZG67vBEgSgMEmah/Ubiw6CEmVx09KHtRK+U3Hyl50r46xys
- KFLE16L+lopQewp6EvInExd35IE+4KMbyN2zNYjAP+viXUotJFkDLWYHXg67xQdWyT86QcmoWLe
- RgPgSuoaa2XdeNFOEIuKBMRd6OKguUms+b/OiwgSG+3A==
+ b=V0bD7B0eplOnGaL27lwIsuXgLBsig2ZbEmDJwEwdIfDtCOmyCcgNgOqD9rthM7/Epr9HyrSOy
+ 34oL1XQMfEcXF9pCGELQylaCo5q1L7FmixE3cNr7vkTF+ZVjwMgIfKIkObmbB+q5KBjaI9iGfNP
+ 2KUwvgpbNlJIBMCITQRF3jG7tHDRXOO0Uqp/Nc0d2fm8MGUAlWgD4jx9he09suzhgtpuT/adcEz
+ SgaBXc3WxFepahEko+n/wuoLyCl+bZ6GP5GZtmEUhMkGEgZAQRdm+PmvyJ99QDo6k0TDJuuBxkv
+ lPxEjCyA4/qdNnkQoQ+mFW+zj6fBTVn3SSjSxXGka+gw==
 From: PARTH <parthdegama@cockatielone.biz>
 To: pthreeh@outlook.com
-Subject: Hello Test Message From India
-Message-ID: <1cd003df-76dd-eb31-e228-efaa49e71d94@cockatielone.biz>
-Date: Fri, 09 Aug 2024 11:50:30 +0000
+Subject: Hello, I'm From India!
+Message-ID: <e560e7ed-7e22-9575-23a2-606026b6860a@cockatielone.biz>
+Date: Sat, 10 Aug 2024 02:08:27 +0000
 MIME-Version: 1.0
 Content-Type: multipart/alternative;
- boundary="--_NmP-4da7f6a060b1093c-Part_1"
+ boundary="--_NmP-9a6384c3eb8355d1-Part_1"
 
-----_NmP-4da7f6a060b1093c-Part_1
+----_NmP-9a6384c3eb8355d1-Part_1
 Content-Type: text/plain; charset=utf-8
 Content-Transfer-Encoding: base64
 
-SGkgUGFydGghIGkgYW0gamhvbgoKIGFrYSBsb2wga2luZw==
-----_NmP-4da7f6a060b1093c-Part_1
+SGkgUGFydGghIGkgYW0gS1QKCiBha2EgRGhhcnZp
+----_NmP-9a6384c3eb8355d1-Part_1
 Content-Type: text/html; charset=utf-8
 Content-Transfer-Encoding: base64
 
 PGgxPkhpIFBhcnRoITwvaDE+PGgzPkFrYSBMb2xraW5nPC9oMz4=
-----_NmP-4da7f6a060b1093c-Part_1--
-
+----_NmP-9a6384c3eb8355d1-Part_1--
 
 `
